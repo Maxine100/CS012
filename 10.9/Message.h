4abba5019b2a59d4bf44425f7bb449887cdd9a0b
Maxine Wu <maxine.wu00@gmail.com>
2021-02-12 21:46:32 -0800

    Created 10.9 and added header files.

---
+++ 10.9/Message.h
@@ -0,0 +1,18 @@
+class Message {
+	private:
+		string author;
+		string subject;
+		string body;
+	
+	public:
+		// Default constructor
+		Message();
+
+		// Parameterized constructor
+		Message(const string& authr, const string& sbjct, const string& body);
+
+		// Displays message in following format:
+		// subject
+		// from author: body
+		void display() const;
+};
---
+++ 10.9/User.h
@@ -0,0 +1,26 @@
+class User {
+	private:
+		string username;
+		string password;
+	
+	public:
+		// Creates a user with empty name and password.
+		User();
+
+		// Create a user with given username and password.
+		User(const string& uname, const string& pass);
+
+		// Returns username.
+		string getUsername() const;
+
+		// Returns true if the stored username/password matches with the parameters.
+		// Otherwise returns false. Note that, even though a User with empty name
+		// and password is actually a valid User object (it is the default User),
+		// this function must still return false if given an empty uname string.
+		bool check(const string& uname, const string& pass) const;
+
+		// Sets a new password. This function should only set hte new password if
+		// the current (old) password is passed in. Also, a default User cannot
+		// have its password changed. Returns true if password changed, false if not.
+		bool setPassword(const string& oldpass, const string& newpass);
+};
