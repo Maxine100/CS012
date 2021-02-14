bd20c68509f6b48fcf603bda670873008c0a6f7f
Maxine Wu <maxine.wu00@gmail.com>
2021-02-14 15:23:44 -0800

    Created 12.14.cpp.

---
+++ 12.14.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+char* mystrcat(char* destination, const char* source) {
+	
+	return destination;
+}
+
+int main() {
+	
+	char cstr1[80];
+	char cstr2[80];
+
+	cstr1[0] = 'L';
+	cstr1[1] = 'i';
+	cstr1[2] = 'o';
+	cstr1[3] = 'n';
+	cstr1[4] = 'e';
+	cstr1[5] = 'l';
+	cstr1[6] = '\0';
+
+	cstr2[0] = 'M';
+	cstr2[1] = 'e';
+	cstr2[2] = 's';
+	cstr2[3] = 's';
+	cstr2[4] = 'i';
+	cstr2[5] = '\0';
+
+	cout << cstr1 << endl;
+	cout << cstr2 << endl;
+
+	mystrcat(cstr1, " ");
+	mystrcat(cstr1, cstr2);
+
+	cout << cstr1 << endl;
+
+	return 0;
+}
