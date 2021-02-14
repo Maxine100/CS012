011e839482525038dcdb055b8d41acb8e564239b
Maxine Wu <maxine.wu00@gmail.com>
2021-02-13 22:47:06 -0800

    Created 11.9.cpp.

---
+++ 11.9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+double mean(const double array[], int arraySize) {
+
+}
+
+void display(const double array[], int arraySize) {
+
+}
+
+void remove (double array[], int& arraySize, int index) {
+	
+}
+
+int main() {
+
+	return 0;
+}
