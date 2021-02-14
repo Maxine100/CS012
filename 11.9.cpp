12b318a11481c44fe17032be726d353862d0e4cf
Maxine Wu <maxine.wu00@gmail.com>
2021-02-13 22:51:07 -0800

    Implemented functions.

--- 11.9.cpp
+++ 11.9.cpp
@@ -3,18 +3,28 @@
 using namespace std;
 
 double mean(const double array[], int arraySize) {
-
+	double sum = 0;
+	for (int i = 0; i < arraySize; ++i) {
+		sum = sum + array[i];
+	}
+	return sum / arraySize;
 }
 
 void display(const double array[], int arraySize) {
-
+	for (int i = 0; i < arraySize - 1; ++i) {
+		cout << array[i] << ", ";
+	}
+	cout << array[arraySize - 1];
 }
 
 void remove (double array[], int& arraySize, int index) {
-	
+	for (int i = index; i < arraySize - 1; ++i) {
+		array[i] = array[i + 1];
+	}
+	arraySize = arraySize - 1;
 }
 
 int main() {
-
+	
 	return 0;
 }
