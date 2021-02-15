9d2a136e13d296733d575940cb3922963bfde02a
Maxine Wu <maxine.wu00@gmail.com>
2021-02-14 16:49:24 -0800

    Added more to main.cpp.

--- 12.16/main.cpp
+++ 12.16/main.cpp
@@ -13,5 +13,37 @@ int main() {
 		cout << vec.at(i) << endl;
 	}
 
+	// Calls default constructor
+	IntVector item1;
+
+	// Calls size, capacity, and empty functions.
+	cout << "item1's size: " << item1.size() << endl;
+	cout << "item1's capacity: " << item1.capacity() << endl;
+	if (item1.empty()) {
+		cout << "item1 is empty." << endl;
+	}
+	else {
+		cout << "item1 is not empty." << endl;
+	}
+
+	// Calls other constructor.
+	IntVector item2(4, 100);
+
+	// Calls size, capacity, and empty functions.
+	cout << "item2's size: " << item2.size() << endl;
+	cout << "item2's capacity: " << item2.capacity() << endl;
+	if (item2.empty()) {
+		cout << "item2 is empty." << endl;
+	}
+	else {
+		cout << "item2 is not empty." << endl;
+	}
+
+	// Calls at() function.
+	int index;
+	cout << "Enter index of wanted element for item2: ";
+	cin >> index;
+	cout << item2.at(index) << endl;
+
 	return 0;
 }
