fee0cf1a2d6bcb3fdd073898c3f3c4b940a67fd9
Maxine Wu <maxine.wu00@gmail.com>
2021-02-16 20:05:02 -0800

    Added IntList.cpp.

---
+++ 14.11/IntList.cpp
@@ -0,0 +1,64 @@
+#include "IntList.h"
+
+IntList::IntList() {
+	head = 0;
+}
+
+void IntList::push_front(int value) {
+	IntNode* temp = this->head;
+
+	if (head == 0) {
+		head = new IntNode(value);
+		(*head).next = temp;
+	}
+	else {
+		head = new IntNode(value);
+		(*head).next = temp;
+	}
+}
+
+ostream& operator<<(ostream& out, const IntList& list) {
+	IntNode* curr = list.head;
+
+	if (curr == 0) {
+		return out;
+	}
+	return (out << curr);
+}
+
+bool IntList::exists(int value) const {
+	if (exists(head, value)) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+bool IntList::exists(IntNode* node, int value) const {
+	if (node == 0) {
+		return false;
+	}
+	else if (value == node->data) {
+		return true;
+	}
+	else {
+		return exists(node->next, value);
+	}
+}
+
+ostream& operator<<(ostream& out, IntNode* rhs) {
+	if (rhs == 0) {
+		return out;
+	}
+	else {
+		if (rhs->next != 0) {
+			out << rhs->data << " ";
+		}
+		else {
+			out << rhs->data;
+		}
+		rhs = rhs->next;
+		return (out << rhs);
+	}
+}
