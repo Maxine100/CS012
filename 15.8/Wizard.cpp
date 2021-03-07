ae44972085188003fafbcff34eaf7e3d459c62ac
Maxine Wu <maxine.wu00@gmail.com>
2021-03-06 21:06:56 -0800

    Removed comments and simplified.

--- 15.8/Wizard.cpp
+++ 15.8/Wizard.cpp
@@ -11,26 +11,12 @@ void Wizard::attack(Character& attackee) {
 		Wizard &attackedWizard = dynamic_cast<Wizard&>(attackee);
 		d = attackStrength * this->rank / attackedWizard.rank;
 		attackedWizard.damage(d);
-		cout << "Wizard " << this->name << " attacks " << attackedWizard.name << " --- POOF!!" << endl;
-		cout << attackedWizard.name << " takes " << d << " damage." << endl;
 	}
 	// else if attackee isn't wizard
 	else {
 		d = this->attackStrength;
-		cout << "Wizard " << this->name << " attacks " << attackee.getName() << " --- POOF!!" << endl;
-		cout << attackee.getName() << " takes " << d << " damage." << endl;
 		attackee.damage(d);
 	}
-
-	/* if (opponent.getType() == type) {
-		Wizard &opp = dynamic_cast<Wizard&>(opponent);
-		d = attackStrength * rank / opp.rank;
-		opp.damage(d);
-	}
-	else {
-		d = attackStrength;
-		opponent.damage(d);
-	}
-	cout << "Wizard " << this->name << " attacks " << opponent.getName() << " --- POOF!!" << endl;
-	cout << opponent.getName() << " takes " << d << " damage." << endl; */
+	cout << "Wizard " << this->name << " attacks " << attackee.getName() << " --- POOF!!" << endl;
+	cout << attackee.getName() << " takes " << d << " damage." << endl;
 }
